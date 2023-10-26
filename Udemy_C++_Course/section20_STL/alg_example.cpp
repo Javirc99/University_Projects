8f96e27aad9f7bd3bf74a929f09e3ba2437ffbd0
Francisco Javier Román Cortés <javierromancortes1999@gmail.com>
2023-10-27 00:47:02 +0200

    section20

--- Udemy_C++_Course/section20_STL/alg_example.cpp
+++ Udemy_C++_Course/section20_STL/alg_example.cpp
@@ -4,28 +4,33 @@
 #include <algorithm>
 #include <cctype>
 
-class Person{
+class Person
+{
     std::string name;
     int age;
+
 public:
     Person() = default;
-    Person(std::string name, int age) : name(name), age(age){}
+    Person(std::string name, int age) : name(name), age(age) {}
 
-    bool operator<(const Person &rhs) const {
+    bool operator<(const Person &rhs) const
+    {
         return this->age < rhs.age;
     }
-    bool operator==(const Person &rhs) const{
-        return (this-> name == rhs.name && this -> age == rhs.age);
+    bool operator==(const Person &rhs) const
+    {
+        return (this->name == rhs.name && this->age == rhs.age);
     }
     ~Person() = default;
 };
 
-//Find an element in a container
+// Find an element in a container
 
-void find_test(){
+void find_test()
+{
     std::cout << "\n====================================" << std::endl;
 
-    std::vector<int> vec{1,2,3,4,5};
+    std::vector<int> vec{1, 2, 3, 4, 5};
 
     auto loc = std::find(std::begin(vec), std::end(vec), 7);
 
@@ -34,85 +39,92 @@ void find_test(){
     else
         std::cout << "Couldn't find the number" << std::endl;
 
-
     std::list<Person> players{
         {"Larry", 18},
         {"Moe", 20},
-        {"Curly", 21}
-    };
+        {"Curly", 21}};
 
     auto loc1 = std::find(players.begin(), players.end(), Person{"Moe", 20});
-    if(loc1 != players.end())
+    if (loc1 != players.end())
         std::cout << "Found Moe in the list" << std::endl;
     else
         std::cout << "Didn't find Moe" << std::endl;
-
 }
 
 /////////////////////////////////////////////////////////////////////////////////
-void count_test(){
+void count_test()
+{
     std::cout << "\n====================================" << std::endl;
 
-    std::vector<int> vec{1,2,3,4,5,1,2,1};
+    std::vector<int> vec{1, 2, 3, 4, 5, 1, 2, 1};
 
-    int num = std::count(vec.begin(),vec.end(), 1);
+    int num = std::count(vec.begin(), vec.end(), 1);
 
     std::cout << num << " occurrences found" << std::endl;
 }
 
-void count_if_test(){
+void count_if_test()
+{
     std::cout << "\n====================================" << std::endl;
 
-    //Count only if the element is even
-    std::vector<int> vec{1,2,3,4,5,1,2,1,100};
-    int num = std::count_if(vec.begin(),vec.end(), [](int x) {return x%2 == 0;});
+    // Count only if the element is even
+    std::vector<int> vec{1, 2, 3, 4, 5, 1, 2, 1, 100};
+    int num = std::count_if(vec.begin(), vec.end(), [](int x)
+                            { return x % 2 == 0; });
 
     std::cout << num << " even numbers found" << std::endl;
 
-    num = std::count_if(vec.begin(),vec.end(), [](int x) {return x%2 != 0;});
+    num = std::count_if(vec.begin(), vec.end(), [](int x)
+                        { return x % 2 != 0; });
     std::cout << num << " odd numbers found" << std::endl;
 
     // How can we determine how many elements in vec are >= 5?
-    num = std::count_if(vec.begin(),vec.end(), [](int x){return x>=5;});
+    num = std::count_if(vec.begin(), vec.end(), [](int x)
+                        { return x >= 5; });
     std::cout << num << " numbers are greater or equal than 5" << std::endl;
+}
 
-}   
-
-//Replace ocurrences of elements in a container
-void replace_test(){
+// Replace ocurrences of elements in a container
+void replace_test()
+{
     std::cout << "\n====================================" << std::endl;
 
-    std::vector<int> vec{1,2,3,4,5,1,2,1};
-    for(auto i: vec){
+    std::vector<int> vec{1, 2, 3, 4, 5, 1, 2, 1};
+    for (auto i : vec)
+    {
         std::cout << i << " ";
     }
     std::cout << std::endl;
 
     std::replace(vec.begin(), vec.end(), 1, 100);
 
-    for(auto i: vec){
+    for (auto i : vec)
+    {
         std::cout << i << " ";
     }
     std::cout << std::endl;
-
 }
 
-void all_of_test(){
+void all_of_test()
+{
     std::cout << "\n====================================" << std::endl;
-    std::vector<int> vec1{1,3,5,7,9,1,3,13,19,5};
+    std::vector<int> vec1{1, 3, 5, 7, 9, 1, 3, 13, 19, 5};
 
-    if(std::all_of(vec1.begin(), vec1.end(), [](int x){return x>10;}))
+    if (std::all_of(vec1.begin(), vec1.end(), [](int x)
+                    { return x > 10; }))
         std::cout << "All of the elements in the vector are > 10" << std::endl;
     else
         std::cout << "Not all of the elements in the vector are > 10" << std::endl;
 
-    if(std::all_of(vec1.begin(), vec1.end(), [](int x){return x<20;}))
+    if (std::all_of(vec1.begin(), vec1.end(), [](int x)
+                    { return x < 20; }))
         std::cout << "All of the elements in the vector are < 20" << std::endl;
     else
         std::cout << "Not all of the elements in the vector are < 20" << std::endl;
 }
 
-void string_transform_test(){
+void string_transform_test()
+{
     std::cout << "\n====================================" << std::endl;
 
     std::string str1("This is a test");
@@ -121,10 +133,11 @@ void string_transform_test(){
     std::cout << "After transform: " << str1 << std::endl;
 }
 
-int main(){
+int main()
+{
 
     // find_test();
-    
+
     // count_test();
 
     // count_if_test();
---
+++ Udemy_C++_Course/section20_STL/vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+
+int main()
+{
+    // test1();
+    // test2();
+    // test3();
+    // test4();
+    // test5();
+    // test6();
+    // test7();
+    // test8();
+    test9();
+
+    return 0;
+}
